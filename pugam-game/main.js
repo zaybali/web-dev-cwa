df494c794e3642e7b3198d0a315e32afe1c01576
zaibali <zaibali51@live.com>
2025-02-03 22:53:11 +0500

    feat: implement initial game logic and structure in main.js

---
+++ pugam-game/main.js
@@ -0,0 +1,45 @@
+const MOVES = {
+  FRONT: 'front',
+  BACK: 'back',
+}
+
+const MOVES_SOURCES = {
+  FRONT: './images/front.png',
+  BACK: './images/back.png',
+}
+
+const comp1 = {
+  move: MOVES.FRONT
+}
+
+const comp2 = {
+  move: MOVES.FRONT
+}
+
+const player1 = {
+  move: MOVES.FRONT
+}
+
+/**
+ * page load
+ * Moves Calculate
+ * Render
+ * Who is the winner
+ * Announce the winner
+ */
+
+const calculateMoves = () => {
+  // Randome Computer moves
+  // get user move
+}
+
+const render = () => {
+
+}
+
+const main = () => {
+  calculateMoves();
+  render();
+}
+
+main();
