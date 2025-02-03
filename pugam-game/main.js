6434eea2e7fb0c253eecd4f61636837152207e09
zaibali <zaibali51@live.com>
2025-02-03 23:28:28 +0500

    feat: update game logic to use SVG images and enhance move validation

--- pugam-game/main.js
+++ pugam-game/main.js
@@ -4,8 +4,8 @@ const MOVES = {
 }
 
 const MOVES_SOURCES = {
-  FRONT: './images/front.png',
-  BACK: './images/back.png',
+  FRONT: './images/front.svg',
+  BACK: './images/back.svg',
 }
 
 const comp1 = {
@@ -13,7 +13,7 @@ const comp1 = {
 }
 
 const comp2 = {
-  move: MOVES.FRONT
+  move: MOVES.BACK
 }
 
 const player1 = {
@@ -30,10 +30,57 @@ const player1 = {
 
 const calculateMoves = () => {
   // Randome Computer moves
+  const moves = [MOVES.FRONT, MOVES.BACK];
+  comp1.move = moves[getRandomIndex()];
+  comp2.move = moves[getRandomIndex()];
   // get user move
+  const userMove = prompt('Enter your move: front or back');
+
+  if (userMove === null) {
+    alert('invalid move');
+    throw new Error('invalid move');
+  }
+
+  if (userMove.toLowerCase() !== MOVES.FRONT && userMove.toLowerCase() !== MOVES.BACK) {
+    alert('invalid move');
+    throw new Error('invalid move');
+  }
+  player1.move = userMove.toLowerCase();
+}
+
+const getRandomIndex = () => {
+  return Math.random() > 0.5 ? 1 : 0;
 }
 
 const render = () => {
+  const comp1Img = document.getElementById('comp1');
+  const comp2Img = document.getElementById('comp2');
+  const player1Img = document.getElementById('player1');
+
+  // For Comp1
+  if (comp1.move === MOVES.FRONT) {
+    comp1Img.src = MOVES_SOURCES.FRONT;
+  } else {
+    comp1Img.src = MOVES_SOURCES.BACK;
+  }
+
+  // For Comp2
+  if (comp2.move === MOVES.FRONT) {
+    comp2Img.src = MOVES_SOURCES.FRONT;
+  } else {
+    comp2Img.src = MOVES_SOURCES.BACK;
+  }
+
+  // For Player1
+  if (player1.move === MOVES.FRONT) {
+    player1Img.src = MOVES_SOURCES.FRONT;
+  } else {
+    player1Img.src = MOVES_SOURCES.BACK;
+  }
+
+}
+
+const calculateWinner = () => {
 
 }
 
